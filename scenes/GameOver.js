5d9eecaf97cbe90f96bf989f4897c30259073d27
herc <herculeshorse@tuta.io>
2021-10-24 14:47:50 +0300

    fix positions

--- scenes/GameOver.js
+++ scenes/GameOver.js
@@ -4,11 +4,13 @@ class GameOver extends Phaser.Scene{
   }
 
   create(){
+    const screenCenterX = this.cameras.main.worldView.x + this.cameras.main.width / 2;
+
     var style = { font: "48px pixelFont", fill: "#ffffff", align: "center" };
-    this.add.text(250, 220, "GAME OVER", style).setOrigin(0.5);
-    this.add.text(335, 285, "SCORE : " + points, { font: "20px pixelFont", fill: "#ffffff", align: "center" }).setOrigin(0.5);
-    this.add.text(250, 345, "PRESS [R] TO TRY AGIAN", { font: "20px pixelFont", fill: "#ffffff", align: "center" }).setOrigin(0.5);
-    this.add.text(200, 370, "PRESS [ESC] TO GO TO MAIN MENU", { font: "20px pixelFont", fill: "#ffffff", align: "center" }).setOrigin(0.5);
+    this.add.text(screenCenterX, 200, "GAME OVER", style).setOrigin(0.5);
+    this.add.text(screenCenterX, 295, "SCORE : " + points, { font: "20px pixelFont", fill: "#ffffff", align: "center" }).setOrigin(0.5);
+    this.add.text(screenCenterX, 345, "PRESS [R] TO TRY AGIAN", { font: "20px pixelFont", fill: "#ffffff", align: "center" }).setOrigin(0.5);
+    this.add.text(screenCenterX, 370, "PRESS [ESC] TO GO TO MAIN MENU", { font: "20px pixelFont", fill: "#ffffff", align: "center" }).setOrigin(0.5);
     sfx['music'].stop();
     sfx['music2'].stop();
     sfx['gameover_sfx'].play();
